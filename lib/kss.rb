10ab458bcb45138f285f96159416f04b16be0bff
JohnAlbin <virtually.johnalbin@gmail.com>
2015-01-03 16:29:14 +0800

    Remove all files except spec.

--- lib/kss.rb
+++
@@ -1,9 +0,0 @@
-require 'kss/comment_parser'
-require 'kss/modifier'
-require 'kss/parser'
-require 'kss/section'
-require 'kss/version'
-
-module Kss
-  STYLEGUIDE_PATTERN = (/(?<!No )Styleguide [[:alnum:]]/i).freeze
-end
--- lib/kss/version.rb
+++
@@ -1,3 +0,0 @@
-module Kss
-  VERSION = "0.5.0"
-end
